7e82e2ecd7263295af863a6715c5a91a02e32c18
scott <scott@clobot.co.kr>
2023-05-13 16:44:48 +0900

    [ADD] 유효성 체크 util 추가

---
+++ src/utils/validation.js
@@ -0,0 +1,38 @@
+export const Validation = {
+    validateName: (name) => {
+        const regex = /^[가-힣]{2,}|[a-zA-Z]{3,}$/;
+        return !regex.test(name);
+    },
+    validateEmail: (email) => {
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return !regex.test(email);
+    },
+    validatePhone: (phone) => {
+        const regex = /^0\d{1,2}[-\s]?\d{3,4}[-\s]?\d{4}$/;
+        return !regex.test(phone);
+    },
+    validatePassword(password) {
+        const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+        return !regex.test(password);
+    },
+    validateCardNumber(cardNumber) {
+        return !this.luhnCheck(cardNumber.replace(/\D/g, ''))
+    },
+
+    luhnCheck(cardNumber) {
+        let sum = 0;
+        let isEven = false;
+        for (let i = cardNumber.length - 1; i >= 0; i--) {
+            let digit = parseInt(cardNumber.charAt(i), 10);
+            if (isEven) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit -= 9;
+                }
+            }
+            sum += digit;
+            isEven = !isEven;
+        }
+        return sum % 10 === 0;
+    }
+}
